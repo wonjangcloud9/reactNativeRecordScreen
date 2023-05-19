fe8902b1200124f58fa6aad039f935fd58262c65
wonjangcloud9 <venuscwj@naver.com>
2023-05-19 22:00:45 +0900

    permission

--- App.tsx
+++ App.tsx
@@ -1,8 +1,56 @@
 import React from 'react';
 
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  PermissionsAndroid,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 function App(): JSX.Element {
+  const requestStoragePermission = async () => {
+    try {
+      const grantedStorage = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
+            'Cool Photo App needs access to your camera ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (grantedStorage === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the camera');
+      } else {
+        console.log('Camera permission denied');
+      }
+
+      const grantedForeGround = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.FOREGROUND_SERVICE,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
+            'Cool Photo App needs access to your camera ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (grantedForeGround === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the storage & foreground service');
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   return (
     <SafeAreaView>
       <View>
